1681f4b8f37fb7340e067027a0e0791dfecad702
Neha19 <nehavardekar19@gmail.com>
2021-07-29 16:01:57 +0530

    new

---
+++ jenkinsDemo/src/main/java/com/kira/jenkinsDemo/HelloJenkins.java
@@ -0,0 +1,12 @@
+package com.kira.jenkinsDemo;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+		System.out.println("What's up?");
+		System.out.println("hi!!");
+	}
+
+}

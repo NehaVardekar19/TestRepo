f61eea23860b23f36f2d0e5c5a2592746f384723
Neha19 <nehavardekar19@gmail.com>
2021-07-29 13:18:44 +0530

    hiii

---
+++ JenkinsTest/src/com/kira/HelloJenkins.java
@@ -0,0 +1,11 @@
+package com.kira;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+		System.out.println("What's up?");
+	}
+
+}
